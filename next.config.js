52ccf51519da6cc26bbff3f963a0ca3017625e67
Dinesh Katwal <medineshkatwal@gmail.com>
2018-02-08 16:13:00 +0545

    change env-config

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
     }
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: !debug ? '/docker-cheatsheet/' : '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
